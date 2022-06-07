5629d34365b2fcedfc34d24b98f5b2e2cefe4e0b
Любовь Воронова <darkluba@yandex.ru>
2022-06-07 17:58:40 +0300

    Отправка проекта на 1 ревью

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -3,13 +3,15 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique = True)
+    slug = models.SlugField(unique=True)
     description = models.TextField()
 
     def __str__(self) -> str:
         return self.title
+
 
 class Post(models.Model):
     text = models.TextField()
@@ -19,9 +21,8 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts')
     group = models.ForeignKey(
-        Group, 
+        Group,
         blank=True,
         null=True,
         on_delete=models.CASCADE
-        )
-                                
+    )
